edf4d128507813dc4f8e8ada28bdfcad468c0441
AlysonRodrigo <alysonrodrigo14@hotmail.com>
2022-02-01 20:46:34 -0300

    back end concluido e tela entrar concluido

---
+++ back/src/main/java/com/verzel/Carros/Model/Car.java
@@ -0,0 +1,56 @@
+package com.verzel.Carros.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+public class Car {
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) long idCar;
+	private String marca;
+	private String modelo;
+	private String foto;
+	
+	@ManyToOne
+	@JoinColumn(name = "criadores")
+	@JsonIgnoreProperties({ "minhasPublicacoes" })
+	private Usuario publicador;
+	
+	public long getIdCar() {
+		return idCar;
+	}
+	public void setIdCar(long idCar) {
+		this.idCar = idCar;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+		
+	}
+	public Usuario getPublicador() {
+		return publicador;
+	}
+	public void setPublicador(Usuario publicador) {
+		this.publicador = publicador;
+	}
+	
+}
